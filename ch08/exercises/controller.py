bdd51eb41b5d0df3725853d42f2de47e03ffbd5c
Your Name01~ <you@example.com>
2023-04-24 13:10:09 -0400

    hy

---
+++ ch08/exercises/controller.py
@@ -0,0 +1,7 @@
+import pygame
+from src.cnowman import Snowman
+
+    self.snowpeople=pygame.sprite.Group()
+    self.max_snowpeoples=20
+    number_of_snowpeoples=2
+    interval
--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -4,39 +4,82 @@ class StringUtility:
 
 
     def __str__(self):
+        
         return self.string
     
 
     def vowels(self):
+        """
+        Count the number of vowels in the string and return the result as a string
+        
+        """
         size=len(self.string)
         count=0
         for i in range(0,size):
-            if self.string[i] is "a","e","i","o","u":
+            if self.string[i] in ["a","e","i","o","u"]:
                 count+=1
             
         if count>=5:
             return "many"
         else:
-            return count
+            return str(count)
         
     def bothEnds(self):
+        """
+        return a string made of the first 2 and last 2 characters of the original string,
+        However, if the string length is less than or equal to 2, return an empty string instead.
+        
+        """
         size=len(self.string)
-        new_word=self.string[0]+self.string[1]+self.string[size-1]+ self.string[size]
         if size<=2:
             return ""
-        else:
-            return new_word
+        new_word = self.string[:2]+self.string[-2:]
+        
+        return new_word
         
     def fixStart(self):
-        first_letter=self.string[0]
+        """
+        return a string where all occurrences of the first character have been changed to '*'
+        """
         size=len(self.string)
-        for i in range(1,size):
-            if first_letter == self.string[i]:
-                new_word=self.string[i].replace("*")
         if size<=1:
             return self.string
-        else:
-            return new_word
+        first_letter=self.string[0]
         
-         
+        
+        new_word=self.string[1:].replace(first_letter,"*")
+        new_word=first_letter+new_word
+        return new_word
+        
+    def asciiSum(self):
+        """
+        return an int that contains the sum of all ascii values in the string.
+        """
+        acii_value=0
+        for i in self.string:
+            acii_value+=ord(i)
+
+        return acii_value
+    
+    def cipher(self):
+        """
+        return an encoded string by incrementing all letters by the length of the string.
+        """
+        
+        alpha_list=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
+                    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        size=len(self.string)
+        shift=11
+        new_word=""
+        for i in range(size):
+            idx = (alpha_list.index(self.string[i].upper())+shift) %26
+
+            
+            new_letter=alpha_list[idx]
+            
+
+            new_word+=new_letter
+        
+        return new_word.lower()
             
