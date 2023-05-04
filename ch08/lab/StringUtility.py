ba6c7f8f015f1ded0511545688255e2bb97fb895
Your Name01~ <you@example.com>
2023-05-04 17:03:38 -0400

    hi

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -67,26 +67,27 @@ class StringUtility:
 
     def cipher(self):
         """
-        return an encoded string by incrementing all letters by the length of the string.
+        Encrypts or decrypts a message using the Caesar cipher technique.
+
+        args:
+            text:str = the message to encrypt or decrypt
+            shift:int = the number of positions to shift each letter
+        return:
+            :str = the encrypted or decrypted message
         """
-        
-        alpha_list=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
-                    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',' ']
+
+        result = ""
         size=len(self.string)
-        shift=size
-        new_word=""
-        for i in range(size):
-            idx = (alpha_list.index(self.string[i].upper())+shift) %27
-
-            
-            new_letter=alpha_list[idx]
-            
-
-            new_word+=new_letter
-        
-        return new_word.lower()
-             
+        for char in self.string:
+            if char.isalpha():
+                # Determine the case of the character
+                start = ord('A') if char.isupper() else ord('a')
+                # Calculate the new position of the character after the shift
+                new_pos = (ord(char) - start + size) % 26
+                # Convert the new position back to a character
+                char = chr(start + new_pos)
+            result += char
+        return result
 
 
 
